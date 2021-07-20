4576cac56d5988d59ce511ca67307723ee3fdb48
jhonatan <jhonatanpires636@gmail.com>
2021-07-20 08:20:43 -0300

    Envio de mensagem automatizada utilizando a biblioteca javaEmail

---
+++ src/EnvioEmail/Biblio/AutomatizacaoDoEmail.java
@@ -0,0 +1,8 @@
+package EnvioEmail.Biblio;
+
+public class AutomatizacaoDoEmail {
+
+    static void enviar(String assunto, String textoDoEmail) {
+
+    }
+}
--- src/EnvioEmail/Biblio/UtilizandoBibliotecas.java
+++ src/EnvioEmail/Biblio/UtilizandoBibliotecas.java
@@ -8,14 +8,14 @@ import org.apache.commons.mail.SimpleEmail;
 public class UtilizandoBibliotecas {
     public static void main(String[] args) throws EmailException {
         Email email = new SimpleEmail();
-        email.setHostName("smtp.googlemail.com");
+        email.setHostName("smtp.gmail.com");
         email.setSmtpPort(465);
-        email.setAuthenticator(new DefaultAuthenticator("username", "password"));
+        email.setAuthenticator(new DefaultAuthenticator("username", "password")); // Coloque seu primeiro nome e sua senha do gmail
         email.setSSLOnConnect(true);
-        email.setFrom("user@gmail.com");
-        email.setSubject("TestMail");
-        email.setMsg("This is a test mail ... :-)");
-        email.addTo("foo@bar.com");
+        email.setFrom("jhonatanpires636@gmail.com");
+        email.setSubject("Utilização de biblioteca"); // Titulo do email
+        email.setMsg("This is a test mail ... :-)"); //Mensagem que queira enviar para o email
+        email.addTo("jhonatanpires229@gmail.com"); //Coloque seu email
         email.send();
     }
 }
